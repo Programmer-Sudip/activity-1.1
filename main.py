ea07c2f8cfd786be62c30c4195b0c6a2e84a5deb
Programmer Sudip <sudipmicrosoft259@gmail.com>
2024-09-07 22:17:13 +0600

    list

---
+++ main.py
@@ -0,0 +1,36 @@
+# Empty tuple
+my_tuple = ()
+print(my_tuple)
+
+# Tuple having integers
+my_tuple = (1, 2, 3)
+print(my_tuple)
+
+# tuple with mixed datatypes
+my_tuple = (1, "Hello", 3.4)
+print(my_tuple)
+
+# nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+print(my_tuple)
+
+# Accenssing tuple elements using indexing
+my_tuple = ("p", "e", "r", "m", "i", "t")
+
+print(my_tuple[0])
+print(my_tuple[5])
+
+#nested tuple
+my_tuple = ("mouse", [8, 4, 6], (1, 2, 3))
+
+#nested index
+print(my_tuple[0][3])
+print(my_tuple[1][1])
+
+
+#slicing
+print("Sliced : ", my_tuple[1:4])
+
+#Interating through tuple
+for letter in my_tuple:
+    print("Hello",letter)
